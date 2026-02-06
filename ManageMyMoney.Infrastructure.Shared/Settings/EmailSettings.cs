3d6f7f455d0591e22465158d946e1142ea95e84d
Francisco Daniel Castro Borrome <borrome941@gmail.com>
2026-02-05 20:14:13 -0400

    ....

--- ManageMyMoney.Infrastructure.Shared/Settings/EmailSettings.cs
+++ ManageMyMoney.Infrastructure.Shared/Settings/EmailSettings.cs
@@ -2,12 +2,12 @@ namespace ManageMyMoney.Infrastructure.Shared.Settings;
 
 public record EmailSettings
 {
-    public string SmtpServer { get; init; } = string.Empty;
-    public int SmtpPort { get; init; } = 587;
-    public string SenderEmail { get; init; } = string.Empty;
-    public string SenderName { get; init; } = string.Empty;
-    public string Username { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
-    public bool EnableSsl { get; init; } = true;
-    public string TemplatesPath { get; init; } = "Templates/Email";
+    public string SmtpServer { get; set; } = string.Empty;
+    public int SmtpPort { get; set; } = 587;
+    public string SenderEmail { get; set; } = string.Empty;
+    public string SenderName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool EnableSsl { get; set; } = true;
+    public string TemplatesPath { get; set; } = "Templates/Email";
 }
